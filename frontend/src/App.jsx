b6c344a20a7cf22bd4070d55edafabe5a541ace0
Huyh Huyhu <2509roblox@gmail.com>
2023-10-13 20:12:21 +0700

    admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
 
 
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import HomeScreen from './screens/HomeScreen.jsx'
-import RegisterScreen from './screens/RegisterScreen.jsx'
-import LoginScreen from './screens/LoginScreen.jsx'
-import CartScreen from './screens/CartScreen.jsx'
-import CheckoutScreen from './screens/CheckoutScreen.jsx'
-import ProfieScreen from './screens/ProfieScreen.jsx'
-import ProductScreen from './screens/ProductScreen.jsx'
-import InvoiceScreen from './screens/InvoiceScreen.jsx'
-import OrderSuccessScreen from './screens/OrderSuccessScreen.jsx';
+import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
+import HomeScreen from './client/HomeScreen.jsx'
+import RegisterScreen from './client/RegisterScreen.jsx'
+import LoginScreen from './client/LoginScreen.jsx'
+import CartScreen from './client/CartScreen.jsx'
+import CheckoutScreen from './client/CheckoutScreen.jsx'
+import ProfileScreen from './client/ProfieScreen.jsx'
+import ProductScreen from './client/ProductScreen.jsx'
+import InvoiceScreen from './client/InvoiceScreen.jsx'
+import OrderSuccessScreen from './client/OrderSuccessScreen.jsx';
+import LoginAdminScreen from './admin/LoginAdminScreen.jsx';
+import DashboardScreen from './admin/DashboardScreen.jsx';
 
 function App() {
 
@@ -18,33 +20,17 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={
-            <HomeScreen />
-          } />
-          <Route path="/login" element={
-            <LoginScreen />
-          } />
-          <Route path="/register" element={
-            <RegisterScreen />
-          } />
-          <Route path="/cart" element={
-            <CartScreen />
-          } />
-          <Route path="/checkout" element={
-            <CheckoutScreen />
-          } />
-          <Route path="/profile" element={
-            <ProfieScreen />
-          } />
-          <Route path="/invoice" element={
-            <InvoiceScreen />
-          } />
-          <Route path="/order-success" element={
-            <OrderSuccessScreen />
-          } />
-          <Route path="/product" element={
-            <ProductScreen />
-          } />
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="/register" element={<RegisterScreen />} />
+          <Route path="/cart" element={<CartScreen />} />
+          <Route path="/checkout" element={<CheckoutScreen />} />
+          <Route path="/profile" element={<ProfileScreen />} />
+          <Route path="/invoice" element={<InvoiceScreen />} />
+          <Route path="/order-success" element={<OrderSuccessScreen />} />
+          <Route path="/product" element={<ProductScreen />} />
+          <Route path="/admin" element={<DashboardScreen />} />
+          <Route path="/admin/login" element={<LoginAdminScreen />} />
 
         </Routes>
       </BrowserRouter>
